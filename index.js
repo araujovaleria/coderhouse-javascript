82028467f265379ed9c80ddc5b58438216f90eea
Valéria de Araújo <araujovaleria.arte@gmail.com>
2024-03-21 21:19:35 -0300

    exercicio aula 01 js

---
+++ index.js
@@ -0,0 +1,20 @@
+// let nome = "Valéria";
+// let sobrenome = "Araújo";
+// let ano = 2024;
+// let idade = 39;
+
+// let contaAno = ano - idade;
+
+// console.log(nome + " " + sobrenome);
+// console.log(contaAno);
+
+let n1 = parseInt(prompt("Digite o primeiro numero"));
+let n2 = parseInt(prompt("Digite o segundo numero"));
+
+let soma = n1 + n2;
+
+console.log(soma);
+
+let multiplicacao = n1 * n2;
+
+alert("O resultado da multipilicação é: " + multiplicacao);
